7be85dc1daa8d63ee2a34832cb577cd3c745ed34
Kuba <6567287+jakubsuchybio@users.noreply.github.com>
2025-02-11 19:34:05 +0100

    Implement Step 1 - models.rs

--- 4. Projects/1. CLI/Problem/Stage 1/Step 1/src/models.rs
+++ 4. Projects/1. CLI/Problem/Stage 1/Step 1/src/models.rs
@@ -1,28 +1,49 @@
+use std::collections::HashMap;
+
 pub enum Status {
-    // TODO: add fields (make sure the fields are public)
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
 }
 
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u64>,
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: Vec::new(),
+        }
     }
 }
 
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    pub name: String,
+    pub description: String,
+    pub status: Status,
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
     }
 }
 
 pub struct DBState {
     // This struct represents the entire db state which includes the last_item_id, epics, and stories
-    // TODO: add fields (make sure the fields are public)
+    pub last_item_id: u64,
+    pub epics: HashMap<u64, Epic>,
+    pub stories: HashMap<u64, Story>,
 }
