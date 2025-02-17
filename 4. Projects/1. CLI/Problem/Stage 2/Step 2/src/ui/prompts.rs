a53e1216bd45b006081f97a67975a35b292f4dd6
Kuba <6567287+jakubsuchybio@users.noreply.github.com>
2025-02-17 13:56:07 +0100

    Implement Stage 2 Step 2

--- 4. Projects/1. CLI/Problem/Stage 2/Step 2/src/ui/prompts.rs
+++ 4. Projects/1. CLI/Problem/Stage 2/Step 2/src/ui/prompts.rs
@@ -21,21 +21,50 @@ impl Prompts {
 }
 
 fn create_epic_prompt() -> Epic {
-    todo!();
+    println!("----------------------------");
+    println!("Epic Name:");
+    let name = get_user_input();
+    println!("Epic Description:");
+    let description = get_user_input();
+
+    Epic::new(name, description)
 }
 
 fn create_story_prompt() -> Story {
-    todo!();
+    println!("----------------------------");
+    println!("Story Name:");
+    let name = get_user_input();
+    println!("Story Description:");
+    let description = get_user_input();
+
+    Story::new(name, description)
 }
 
 fn delete_epic_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!("Are you sure you want to delete this epic? All stories in this epic will also be deleted [Y/n]:");
+    let answer = get_user_input();
+
+    answer == "Y"
 }
 
 fn delete_story_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!("Are you sure you want to delete this story? [Y/n]:");
+    let answer = get_user_input();
+
+    answer == "Y"
 }
 
 fn update_status_prompt() -> Option<Status> {
-    todo!();
+    println!("----------------------------");
+    println!("New Status (1 - OPEN, 2 - IN-PROGRESS, 3 - RESOLVED, 4 - CLOSED):");
+    let answer = get_user_input();
+    match answer.trim().parse::<u8>().ok()? {
+        1 => Some(Status::Open),
+        2 => Some(Status::InProgress),
+        3 => Some(Status::Resolved),
+        4 => Some(Status::Closed),
+        _ => None,
+    }
 }
